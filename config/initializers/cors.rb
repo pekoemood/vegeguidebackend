4f5bf3a237b1a580740505472f6c374aa5c13ef5
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-06 14:18:52 +0900

    cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://vegeguidefrontend.vercel.app/' : "*"
+    origins Rails.env.production? ? 'https://vegeguidefrontend.vercel.app' : "*"
 
     resource "*",
       headers: :any,
