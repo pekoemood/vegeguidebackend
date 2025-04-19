98561851ecbe88b9c9cd82404acde946ac860c0c
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-19 22:26:49 +0900

    todosの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_19_073807) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_19_132521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,13 +24,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_19_073807) do
     t.datetime "updated_at", null: false
     t.index ["vegetable_id", "market", "date"], name: "index_prices_on_vegetable_id_and_market_and_date", unique: true
     t.index ["vegetable_id"], name: "index_prices_on_vegetable_id"
-  end
-
-  create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "vegetables", force: :cascade do |t|
