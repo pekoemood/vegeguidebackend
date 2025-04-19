e98acb3282fe9d90bb7bbc365aea61f03f02728b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-19 23:03:53 +0900

    seedを再度修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'csv'
 csv_file_path = Rails.root.join('db', 'vegeguide.csv') 
 
 CSV.foreach(csv_file_path, headers: true, encoding: 'bom|utf-8') do |row|
-    Vegetable.create!(
+    Vegetable.find_or_create_by!(
       name: row['name'],
       description: row['description'],
       origin: row['origin'],
