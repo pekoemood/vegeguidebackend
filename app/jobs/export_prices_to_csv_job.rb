053752059e7279e8fe9ea9c502208d17dd12c03b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-26 15:35:57 +0900

    priceモデルの出力をjobに設定

---
+++ app/jobs/export_prices_to_csv_job.rb
@@ -0,0 +1,14 @@
+class ExportPricesToCsvJob < ApplicationJob
+  queue_as :default
+  require 'csv'
+
+  def perform
+    prices = Price.all
+
+    CSV.open('db/backup/prices_backup.csv', 'w', write_headers: true, headers: Price.attribute_names) do |csv|
+      prices.find_each do |price|
+        csv << price.attributes.values
+      end
+    end
+  end
+end
