5a994bd9f371ccc4f2abb459d5301f87814dd197
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-26 10:35:16 +0900

    recipe_generationsでリクエストの野菜名を取得

--- app/controllers/api/v1/recipe_generations_controller.rb
+++ app/controllers/api/v1/recipe_generations_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipeGenerationsController < ApplicationController
   def create
-    api_response = GeminiClient.new.generate_recipe
+    vegetable_name = params[:vegetable]
+    api_response = GeminiClient.new(vegetable_name).generate_recipe
     render json: api_response
   end
 end
