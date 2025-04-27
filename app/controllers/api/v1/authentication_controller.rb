baecd0c34b8b23c7737930543e074f8050591720
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-04-27 16:32:29 +0900

    userコントローラーのエンドポイント作成

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::AuthenticationController < ApplicationController
+  require 'jwt'
+  def login
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = TokenGenerator.encode(user.id)
+      cookies[:jwt] = {
+        value: token,
+        expires: 24.hours.from_now,
+        secure: Rails.env.production?,
+        httponly: true,
+        same_site: :none
+      }
+      render json: { name: user.name, email: user.email  }
+    else
+      render json: { status: "メールアドレスかパスワードが間違っています。" }, status: :unprocessable_entity
+    end
+  end
+
+  def logout
+    cookies.delete(:jwt)
+    render json: { message: 'ログアウトしました' }, status: :ok
+  end
+
+  def show_request
+    token = cookies[:jwt]
+    decode = TokenGenerator.decode(token)['token']
+    user = User.find_by(id: decode)
+
+
+    render json: { name: user.name, email: user.email }
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,19 +2,24 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save 
-      token = TokenGenerator.encode(user) if user.save
-      
+
+      token = TokenGenerator.encode(user.id)
       cookies[:jwt] = {
         value: token,
-        expire: 24.hours.from_now,
+        expires: 24.hours.from_now,
         secure: Rails.env.production?,
-        httponly: true
+        httponly: true,
+        same_site: :none
       }
-    render json: { status: "ユーザーの登録に成功しました" }
+
+      render json: { status: "ユーザーの登録に成功しました" }
     else
       render json: { error: "ユーザーの登録に失敗しました" }, status: :unprocessable_entity
     end
   end
+
+
+
 
   private 
 
