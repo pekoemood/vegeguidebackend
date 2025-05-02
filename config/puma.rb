9463e3cf0af93f234da48f6ca4ccd9f892f34bc0
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-02 15:35:14 +0900

    pumaの設定を修正

--- config/puma.rb
+++ config/puma.rb
@@ -35,7 +35,7 @@ pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 # config/puma.rb
 
-if Rails.env.development?
+if ENV["RAILS_ENV"] == "development"
   ssl_bind '0.0.0.0', '3000', {
     key: "/certs/localhost-key.pem",
     cert: "/certs/localhost.pem",
