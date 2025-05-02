3008f4b54dfe0243a49f9b2b2374c586427d26fc
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-02 15:24:13 +0900

    買い物リストindexAPIの微修正

--- app/serializers/shopping_list_serializer.rb
+++ app/serializers/shopping_list_serializer.rb
@@ -3,6 +3,10 @@ class ShoppingListSerializer
   
   attributes :id, :name
 
+  attribute :created_at_jst do |object|
+    object.created_at.in_time_zone('Tokyo').strftime('%Y年%m月%d日')
+  end
+
   attribute :shopping_items do |shopping_list|
     shopping_list.shopping_list_items.map do |item|
       {
