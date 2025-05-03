ef248e656310c3376d44769800990bbbff4a77a6
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-03 20:57:38 +0900

    updateアクションの実装

--- app/controllers/api/v1/shopping_lists_controller.rb
+++ app/controllers/api/v1/shopping_lists_controller.rb
@@ -22,9 +22,32 @@ class Api::V1::ShoppingListsController < ApplicationController
     end
   end
 
+  def update
+    ActiveRecord::Base.transaction do
+      shopping_list_items = @current_user.shopping_lists.find_by(id: params[:id]).shopping_list_items
+      update_items = update_items_params.index_by {|item| item["id"]}
+  
+      shopping_list_items.each do |item|
+        if update_items[item.ingredient_id]
+          checked_value = update_items[item.ingredient_id]["checked"]
+          item.update!(checked: checked_value)
+        end
+      end
+    end
+    render json: {success: "更新に成功しました"}, status: :ok
+  rescue => e
+    render json: { error: "エラーが発生しました: #{e.message}" }, status: :internal_server_error
+  end
+
   private 
 
   def recipe_params
     params.permit(:name, :instructions, :cooking_time, :difficulty, :servings, ingredients: [:name, :amount, :unit], step: [:step_number, :description])
+  end
+
+  def update_items_params
+    params.require(:items).map do |item|
+      item.permit(:id, :checked)
+    end
   end
 end
