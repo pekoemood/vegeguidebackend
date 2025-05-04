40c19f701fb7c7ac4e292e1dd3c5b1c6d7eef22a
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-04 17:27:54 +0900

    レシピコントローラーの作成とindexアクションの実装

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   before_action :authenticate_user!
 
-
+  def index
+    user_recipe = @current_user.recipes
+    render json: RecipeSerializer.new(user_recipe).serializable_hash.to_json
+  end
 
 
 
---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,24 @@
+class RecipeSerializer
+  include JSONAPI::Serializer
+
+  attributes :id, :name, :instructions, :cooking_time, :servings
+
+  attribute :ingredients do |recipe|
+    recipe.ingredients.map do |ingredient|
+      {
+        name: ingredient.name,
+        amount: ingredient.amount,
+        unit: ingredient.unit,
+      }
+    end
+  end
+
+  attribute :recipe_steps do |recipe|
+    recipe.recipe_steps.map do |step|
+      {
+        step_number: step.step_number,
+        description: step.description,
+      }
+    end
+  end
+end
