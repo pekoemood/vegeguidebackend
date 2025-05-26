79831dfa88ca07d54e47a4c2b87d0fc9d92ef0cb
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-05-26 16:43:05 +0900

    レシピから買い物リストへの材料追加機能の実装完了

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -3,4 +3,10 @@ class ShoppingList < ApplicationRecord
   has_many :shopping_list_items, dependent: :destroy
   has_many :recipes, through: :shopping_list_items
   has_many :ingredients, through: :shopping_list_items
+
+  def updated_days_ago
+    return nil if updated_at.nil?
+    days = (Time.zone.today - updated_at.to_date).to_i
+    days.zero? ? '今日' : "#{days}日前"
+  end
 end
