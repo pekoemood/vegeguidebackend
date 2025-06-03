7ee9e4913bcff21727650dd467e94837998998f0
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-06-03 23:08:32 +0900

    本番でcookiesが削除されない問題の解消

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,15 @@ class Api::V1::UsersController < ApplicationController
     if user.save 
 
       token = TokenGenerator.encode(user.id)
+      domain = Rails.env.production? ? 'vegeguidebackend.onrender.com' : nil
       cookies[:jwt] = {
         value: token,
         expires: 24.hours.from_now,
         secure: Rails.env.production?,
         httponly: true,
-        same_site: :none
+        same_site: :none,
+        path: '/',
+        domain: domain
       }
 
       render json: { status: "ユーザーの登録に成功しました", name: user.name }
