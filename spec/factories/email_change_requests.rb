2410e2f5af4717943af8c441f2a57a9fa6637bd7
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-08-07 23:37:39 +0900

    email_change_requesetsテストの実装完了

--- spec/factories/email_change_requests.rb
+++ spec/factories/email_change_requests.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :email_change_request do
     new_email { 'new@example.com' }
     token { 'test' }
-    expires_at { Date.today }
+    expires_at { Date.tomorrow }
     association :user
   end
 end
